ea432296f28fe0ec42e1da858fcaa4e036e2580e
Aruminium <b2201680@photon.chitose.ac.jp>
2022-05-01 16:57:07 +0900

    linear

--- work03/3-2/linear.c
+++ work03/3-2/linear.c
@@ -9,16 +9,7 @@ double Y[N];
 
 int main(void){
 	for(int i= 0;i < N;i++)Y[i] = sin(X[i]);
-	double pn = 0;
-	double Nk;
-	for(int i = 0;i < N;i++){
-		Nk = Y[i];
-		for(int j = 0;j < N;j++){
-			if(i == j)continue;
-			Nk *= (x - X[j]);
-			Nk /= (X[i] - X[j]);
-		}
-		pn += Nk;
-	}
-	printf("%f",pn);
+	double y = Y[1] + (Y[2]-Y[1])*(x - X[1]) / (X[2]-X[1]);
+	printf("%f\n", y);
+	return 0;
 }

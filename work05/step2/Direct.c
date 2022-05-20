e6942b54adcccc85191d0fe5ba48f941fb9bd3db
Aruminium <b2201680@photon.chitose.ac.jp>
2022-05-20 15:42:14 +0900

    work5 Step 1 Add Plot File

--- work05/step2/Direct.c
+++ work05/step2/Direct.c
@@ -5,33 +5,105 @@
 //閉区間[a,b]
 const double a = 1.15;
 const double b = 1.20;
+const double step = 0.001;
 
-typedef struct data{
+typedef struct data {
 	int n; //実行回数
-	int x; //初期値
+	double x; //初期値
 	double ans; //推定値
-	data* next;
+	struct data* next;
 }data;
+
+data* head;
 
 double f(double);
 void sort(data,data);
+void insert(data);;
+data* newData(data);
+void deleteAll();
+void deleteHead();
+void view();
 
 
 int main(void){
-	int n = 0;
-	double x = 1.20,xk;
-	double err = 1;
-	while (err > 0.000001){
-		xk = f(x);
-		err = fabs(x-xk);
-		x = xk;
-		n++;
-		printf("err = %f\n", err);
+	int n;
+	double xk, err, ans;
+	data tmp;
+	printf("#x n\n");
+	for(double x=a; x<=b; x+=step){
+		ans = x;
+		n = 0;
+		err = 1;
+		while (err > 0.000001){
+			xk = f(ans);
+			err = fabs(ans-xk);
+			ans = xk;
+			n++;
+		}
+		tmp.n = n;
+		tmp.x = x;
+		tmp.ans = ans;
+		tmp.next = NULL;
+		insert(tmp);
 	}
-	printf("%f\n%d", x,n);
+	view();
+	deleteAll();
 	return 0;
 }
 
 double f(double x){
 	return atan(2 * x);
+}
+
+void insert(data insertData)
+{
+	data* itr = head;
+	data* ndata = newData(insertData);
+	//空の状態
+	if(head == NULL){
+		head = ndata;
+		return;
+	}
+	while(1){
+		//末尾に入れる場合
+		if(itr->next == NULL){
+			itr->next = ndata;
+			return;
+		}
+		itr = itr->next;
+	}
+	return;
+}
+
+data* newData(data newData){
+	data *ndata = (data*)malloc(sizeof(data));
+	ndata->n = newData.n;
+	ndata->x = newData.x;
+	ndata->ans = newData.ans;
+	ndata->next = NULL;
+	return ndata;
+}
+
+void deleteHead(){
+	data* next;
+	if(head != NULL){
+		next = head->next;
+		free(head);
+		head = next;
+	}
+}
+
+void deleteAll(){
+	while(head != NULL){
+		deleteHead();
+	}
+}
+
+void view(){
+	data* itr = head;
+
+	while(itr != NULL){
+		printf("%f %d\n", itr->x, itr->n);
+		itr = itr->next;
+	}
 }
